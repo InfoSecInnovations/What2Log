cc3528a8a33f5f90aed08c21b101b2ae072126f4
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2022-01-10 16:58:14 +0100

    display references, credits, compliance info

--- pages/logs/_log.vue
+++ pages/logs/_log.vue
@@ -5,14 +5,36 @@
       <div>
         <h2 v-for="os of article.source.os" :key="os">{{os}}</h2>
       </div>
-      <AuthorCard :author="article.author" :date="article.createdAt" />
+      <div class="info-block">
+        <AuthorCard :author="article.author" :date="article.createdAt" />
+        <div v-if="article.credits && article.credits.length" class="credits">
+          <p v-for="(credit, index) of article.credits" :key="`credits-${index}`">{{credit}}</p>
+        </div>
+      </div>
+      <div class="info-block">
+        <h4>{{article.default_enabled ? 'Enabled by default' : 'Disabled by default'}}</h4>
+        <h4 v-if="article.source.service">Service: {{article.source.service}}</h4>
+        <h4 v-if="article.source.log">Log type: {{article.source.log}}</h4>
+      </div>
       <p class="info">{{article.collect_reason}}</p>
+      <div class="compliance-block" v-if="article.compliance && Object.keys(article.compliance).length">
+        <h3>Compliance</h3>
+        <div v-for="(value, name) of article.compliance" :key="`compliance-${name}`" class="info-block">
+          <h4>{{name}}</h4>
+          <p>Level: {{value.level}}</p>
+          <p><a :href="value.reference" target="_blank">{{value.reference}}</a></p>
+        </div>
+      </div>
     </div>
     <div class="columns">
       <template v-for="(section, index) of content">
         <div :key="`enable-txt-${index}`" v-html="$md.render(section.txt)"></div>
         <img :key="`enable-img-${index}`" :src="`/images/${section.img}`">
       </template>
+    </div>
+    <div class="info-block" v-if="article.references && article.references.length">
+      <h3>Additional References</h3>
+      <p v-for="(ref, index) of article.references" :key="`reference-${index}`"><a :href="ref" target="_blank">{{ref}}</a></p>
     </div>
     <div v-if="article.log_pile.view_logs || article.log_pile.check_status || article.log_pile.enable_logs || article.log_pile.disable_logs" class="logpile-section">
       <LogpileScript v-if="article.log_pile.view_logs" :script="article.log_pile.view_logs" script_type="view" :script_language="article.log_pile.language" :oses="article.source.os" :slug="article.slug"/>
