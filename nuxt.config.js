900bbaca4aa3a7ef50a192d64c6951f35d68a281
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2022-01-14 19:58:48 +0100

    use directory from config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import TOML from '@ltd/j-toml'
 import remark from 'remark'
 import strip from 'strip-markdown'
+import fs from 'fs-extra'
 
 const excerptLength = 150
 
@@ -12,95 +13,101 @@ const stripMarkdown = markdown => new Promise((resolve, reject) => {
   })
 })
 
-export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+export default async () => { 
+  const config = await fs.readJSON('w2lconfig.json').catch(() => null)
+  return {
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: 'What2Log - %s',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-    ]
-  },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/main'
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '~/plugins/v-tooltip.js',
-    '~/plugins/filters.js'
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
-    'nuxt-i18n',
-    '@nuxtjs/markdownit'
-  ],
-
-  // Content module configuration: https://go.nuxtjs.dev/config-content
-  content: { 
-    extendParser: {
-      '.toml': file => TOML.parse(file, 1.0, '\n', false)
-    },
-    markdown: {
-      remarkPlugins: ['remark-unwrap-images']
-    }
-  },
-
-  // i18n configuration: https://i18n.nuxtjs.org/options-reference
-  i18n: {
-    locales: [
-      {
-        code: 'en',
-        iso: 'en-US',
-        name: 'English',
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+      titleTemplate: 'What2Log - %s',
+      htmlAttrs: {
+        lang: 'en'
       },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      ]
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+      '~/assets/main'
     ],
-    defaultLocale: 'en',
-    noPrefixDefaultLocale: true,
-  },
 
-  // markdownit configuration
-  markdownit: {
-    linkify: true
-  },
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+      '~/plugins/v-tooltip.js',
+      '~/plugins/filters.js'
+    ],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  hooks: {
-    'content:file:beforeInsert': async (document) => { 
-      if (document.extension === '.md') {
-        const text = document.description ? `${document.description} ${document.text}` : document.text
-        const stripped = await stripMarkdown(text)
-        document.excerpt = stripped.length < excerptLength ? stripped : stripped.substr(0, stripped.lastIndexOf(' ', excerptLength))
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+      // https://go.nuxtjs.dev/content
+      '@nuxt/content',
+      'nuxt-i18n',
+      '@nuxtjs/markdownit'
+    ],
+
+    // Content module configuration: https://go.nuxtjs.dev/config-content
+    content: { 
+      extendParser: {
+        '.toml': file => TOML.parse(file, 1.0, '\n', false)
+      },
+      markdown: {
+        remarkPlugins: ['remark-unwrap-images']
       }
-      if (document.extension === '.toml') {
-        if (document.collect_reason) document.excerpt = document.collect_reason.length < excerptLength ? document.collect_reason : document.collect_reason.substr(0, document.collect_reason.lastIndexOf(' ', excerptLength))
+    },
+
+    // i18n configuration: https://i18n.nuxtjs.org/options-reference
+    i18n: {
+      locales: [
+        {
+          code: 'en',
+          iso: 'en-US',
+          name: 'English',
+        },
+      ],
+      defaultLocale: 'en',
+      noPrefixDefaultLocale: true,
+    },
+
+    // markdownit configuration
+    markdownit: {
+      linkify: true
+    },
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+    },
+
+    generate: {
+      dir: (config && config.dir) || 'dist'
+    },
+
+    hooks: {
+      'content:file:beforeInsert': async (document) => { 
+        if (document.extension === '.md') {
+          const text = document.description ? `${document.description} ${document.text}` : document.text
+          const stripped = await stripMarkdown(text)
+          document.excerpt = stripped.length < excerptLength ? stripped : stripped.substr(0, stripped.lastIndexOf(' ', excerptLength))
+        }
+        if (document.extension === '.toml') {
+          if (document.collect_reason) document.excerpt = document.collect_reason.length < excerptLength ? document.collect_reason : document.collect_reason.substr(0, document.collect_reason.lastIndexOf(' ', excerptLength))
+        }
       }
     }
   }
-
 }
