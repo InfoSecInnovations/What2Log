758b1034e59d298f5b24b66d75c523f4935aaf06
Sebastian Boutin Blomfield <7989595+sebovzeoueb@users.noreply.github.com>
2022-03-31 16:07:38 +0200

    search results page, dropdown improvements

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,10 +5,19 @@
     <NuxtLink to="/about/">About Us</NuxtLink>
     <NuxtLink to="/logpile/">The Log Pile</NuxtLink>
     <NuxtLink to="/blog/">Log Blog</NuxtLink>
-    <input v-model="query" type="search" autocomplete="off"/>
-    <ul v-if="results.length" class="search-result-dropdown">
-      <li v-for="result of results" :key="result.slug">
-        <NuxtLink :to="result.path.replace(`${$i18n.locale}/`, '')" v-on:click.native="resetQuery">{{result.title}}</NuxtLink>
+    <input v-model="query" type="search" autocomplete="off" v-on:click.stop="searchFocus = !searchFocus"/>
+    <ul class="search-result-dropdown" v-if="searchFocus" id="result-dropdown">
+      <li v-for="result of results" :key="result.path">
+        <NuxtLink :to="result.path.replace(`${$i18n.locale}/`, '')" v-on:click.native="resetQuery">
+          <p>{{result.title}}</p>
+          <p class="search-meta">{{`${result.dir.endsWith('logs') ? `${result.source.os.join(' / ')} Logs` : `${result.operating_system.join('/ ')} Tools`}`}}</p>
+        </NuxtLink>
+      </li>
+      <li v-if="results.length">
+        <NuxtLink :to="{path: '/search', query: { query }}" v-on:click.native="resetQuery">See all results...</NuxtLink>  
+      </li>
+      <li>
+        <NuxtLink to="/tags" v-on:click.native="resetQuery">Browse tags...</NuxtLink>  
       </li>
     </ul>
   </div>
@@ -19,7 +28,8 @@ export default {
   data () {
     return {
       query: '',
-      results: []
+      results: [],
+      searchFocus: false
     }
   },
   watch: {
@@ -30,18 +40,24 @@ export default {
       }
 
       this.results = await this.$content(`${this.$i18n.locale}`, {deep: true})
-      .only('title', 'path', 'dir')
+      .where({dir: {$in: [`/${this.$i18n.locale}/logs`, `/${this.$i18n.locale}/tools`]}})
       .sortBy('createdAt', 'asc')
       .limit(12)
       .search(query)
       .fetch()
+
     }
   },
   methods: {
     resetQuery() {
       this.query = ''
-      console.log('yes')
+      this.searchFocus = false
     }
+  },
+  mounted () {
+    document.addEventListener('click', e => {
+      this.searchFocus = false
+    })
   }
 }
 </script>
