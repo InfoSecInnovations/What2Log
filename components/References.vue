ec87ba76d83f59698dc7e4b2150cd7f9828d2061
Sebastian Boutin Blomfield <7989595+sebovzeoueb@users.noreply.github.com>
2022-07-27 11:11:02 +0200

    add GitHub icon to link

--- components/References.vue
+++ components/References.vue
@@ -4,7 +4,10 @@
     <template v-if="article.references">
       <p v-for="(ref, index) of article.references" :key="`reference-${index}`"><a :href="ref" target="_blank">{{ref}}</a></p>
     </template>
-    <a :href="`https://github.com/InfoSecInnovations/What2Log/blob/main/content/${article.path}${article.extension}`" target="_blank"> View this article on GitHub</a>
+    <a :href="`https://github.com/InfoSecInnovations/What2Log/blob/main/content/${article.path}${article.extension}`" target="_blank" class="social">
+      <img src="/images/github_icon.svg" />
+      <div> View this article on GitHub </div>
+    </a>
   </div>
 </template>
 
